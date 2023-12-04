54431c83537775e9a3bde86ccc65873cece2149f
Paulo Araujo <paulofelipe_jau7654@hotmail.com>
2023-12-04 15:01:56 -0300

    feat: solve day 3

---
+++ day3/part2.lua
@@ -0,0 +1,60 @@
+local calculate_total = function(mapp)
+    local nums = {}
+    local symbols = {}
+    for row = 1, #mapp, 1 do
+        for num, pos in string.gmatch(mapp[row], "(%d+)()") do
+            table.insert(nums, {
+                value = tonumber(num),
+                row = row,
+                pos_ini = pos - #num,
+                pos_end = pos - 1,
+            })
+        end
+
+        for symbol, pos in string.gmatch(mapp[row], "(*)()") do
+            table.insert(symbols, {
+                value = symbol,
+                row = row,
+                pos_ini = pos - 1,
+                pos_end = pos - 1,
+            })
+        end
+    end
+
+    local is_adjacent = function(num, symbol)
+        if symbol.row < num.row - 1 or symbol.row > num.row + 1 then
+            return false
+        end
+
+        return symbol.pos_ini >= num.pos_ini - 1
+            and symbol.pos_end <= num.pos_end + 1
+    end
+
+    local total = 0
+    for _, symbol in ipairs(symbols) do
+        local ad = {}
+        for _, num in ipairs(nums) do
+            if is_adjacent(num, symbol) then
+                table.insert(ad, num.value)
+            end
+        end
+
+        if #ad == 2 then
+            total = total + (ad[1] * ad[2])
+        end
+    end
+
+    return total
+end
+
+local mapp = {}
+while true do
+    local line = io.read("L")
+    if line == nil then
+        break
+    end
+
+    table.insert(mapp, line)
+end
+
+print(calculate_total(mapp))
